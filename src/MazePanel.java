2958bf62e0ee2ddfc0adba4a984ebbb8ebce96ee
leon-ng9 <leon.ng9@outlook.com>
2016-05-12 17:28:41 +1000

    Abstract Maze from MazePanel

--- src/MazePanel.java
+++ src/MazePanel.java
@@ -3,122 +3,67 @@ import java.awt.event.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class MazePanel extends JPanel implements ActionListener {
-	private boolean[][] maze;
-	private final int mazeSize = 15;
-	private Player player;
+public class MazePanel extends JPanel implements ActionListener, MazeConstants {
+	private Maze maze;
 	private Timer timer;
-	
-	// constants
-	private final int MAZE_CELL_SIZE = 32;
 	private final int REFRESH_TIME = 10;
-	
+
 	public MazePanel() {
 		initMazePanel();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO: clean everything
-		
-		requestFocus(); // TODO: fix this hack
-		
-		if (isLegalMove(player.getDX(), player.getDY())) {
-			player.move();
-		} else if (player.getDX() != 0 && player.getDY() != 0) {
-			// player is holding two arrow keys so check if just activating one of the two makes the move legal
-			if (isLegalMove(0, player.getDY())) {
-				// move in Y-axis direction only
-				player.manualMove(0, player.getDY());
-			} else if (isLegalMove(player.getDX(), 0)) {
-				// move in X-axis direction only
-				player.manualMove(player.getDX(), 0);
-			}
-		}
-		
+		requestFocus(); // TODO: fix this hack (disable focus for all other elements)
+
+		maze.updateSprites(e);
+
 		repaint();
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
-		
-		// paint the maze
-		Image stoneTile = (new ImageIcon("images/32_stone.png")).getImage();
-		Image fireTile = (new ImageIcon("images/32_mountain.png")).getImage();
-		
-		int size = MAZE_CELL_SIZE;
-		for (int i=0; i<maze.length; i++) {
-			for (int j=0; j<maze[0].length; j++) {
-				if (maze[i][j] == true) {
-					g.drawImage(stoneTile, j * size, i * size, this);
+
+		Image pathTile = maze.getPathTile();
+		Image wallTile = maze.getWallTile();
+
+		boolean[][] mazeGrid = maze.getGrid();
+		for (int i = 0; i < mazeGrid.length; i++) {
+			for (int j = 0; j < mazeGrid[i].length; j++) {
+				if (mazeGrid[i][j]) {
+					g.drawImage(pathTile, j * MAZE_CELL_SIZE, i * MAZE_CELL_SIZE, this);
 				} else{
-					g.drawImage(fireTile, j * size, i * size, this);
+					g.drawImage(wallTile, j * MAZE_CELL_SIZE, i * MAZE_CELL_SIZE, this);
 				}
 			}
 		}
-		
+
 		// paint the player
+		Player player = maze.getPlayer();
 		g.drawImage(player.getImage(), player.getX(), player.getY(), this);
 	}
-	
+
 	private void initMazePanel() {
-		MazeGenerator mazeGenerator = new MazeGenerator();
-		maze = mazeGenerator.generateMaze(mazeSize);
+		maze = new Maze();
 		
-		player = new Player();
 		addKeyListener(new TAdapter());
-		
+
 		timer = new Timer(REFRESH_TIME, this);
 		timer.start();
 	}
-	
-	/**
-	 * Checks whether a player will intersect with a maze wall based on where they want to move
-	 * @param playerDX the wanted x-axis movement
-	 * @param playerDY the wanted y-axis movement
-	 * @return
-	 */
-	private boolean isLegalMove(int playerDX, int playerDY) {
-		for (int i=0; i<maze.length; i++) {
-			for (int j=0; j<maze[0].length; j++) {
-				if (maze[i][j] == false) {
-					// wall 
-					int xIndex = j * MAZE_CELL_SIZE;
-					int yIndex = i * MAZE_CELL_SIZE;
-					Rectangle wallRect = new Rectangle(xIndex, yIndex, MAZE_CELL_SIZE, MAZE_CELL_SIZE);
-					
-					// player
-					int playerWidth = player.getImage().getWidth(this);
-					int playerHeight = player.getImage().getHeight(this);
-					Rectangle playerRect = new Rectangle(player.getX(), player.getY(), playerWidth, playerHeight);
-					playerRect.translate(playerDX, playerDY);
-					
-					// check if wall and player intersection or if player outside of maze
-					if (wallRect.intersects(playerRect) 
-							|| playerRect.getX() < 0 
-							|| playerRect.getX() >= maze[0].length * MAZE_CELL_SIZE - playerWidth
-							|| playerRect.getY() < 0 
-							|| playerRect.getY() >= maze.length * MAZE_CELL_SIZE - playerHeight) {
-						
-						return false;
-					}
-				} 
-			}
-		}
-		
-		return true;
-	}
-	
-	private class TAdapter extends KeyAdapter {
-        @Override
-        public void keyReleased(KeyEvent e) {
-            player.keyReleased(e);
-        }
 
-        @Override
-        public void keyPressed(KeyEvent e) {
-            player.keyPressed(e);
-        }
-    }
+	private class TAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			maze.getPlayer().keyPressed(e);
+		}  
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			maze.getPlayer().keyReleased(e);
+		}
+
+
+	}
 }
