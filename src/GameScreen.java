ed344cff6308661fa96c42fd439bdd1a5025858a
Obvman <obvman@gmail.com>
2016-05-17 00:09:07 +1000

    grid bag initial setup, needs tweaking

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 @SuppressWarnings("serial")
 public class GameScreen extends JPanel implements ActionListener {
@@ -17,6 +19,9 @@ public class GameScreen extends JPanel implements ActionListener {
 	// status bar components
 	JPanel statusBar;
 	
+	// side menu bar
+	JPanel sideMenu;
+	
 	private float duration;
 	private int currLevel;
 	private Timer timer;
@@ -24,19 +29,112 @@ public class GameScreen extends JPanel implements ActionListener {
 	public GameScreen(MainWindow mainWindow) {
 		this.mainWindow = mainWindow;
 		
+		this.setLayout(new GridBagLayout());
+		this.setSize(1200,600);
+		
 		// allow this panel to add inner panels using BorderLayout
-		setLayout(new BorderLayout());
-		setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+//		setLayout(new BorderLayout());
+//		setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
 		// maze panel
 		mazePanel = new JPanel(new CardLayout());
-		add(mazePanel);
+		//add(mazePanel);
 		initMazeScorePanel();
 		initMazeLostPanel();
 		
 		// status bar panel and side menu panel
 		initStatusBar();
 		initSideMenu();
+		
+		// initialise layout
+		GridBagConstraints gbcMaze = new GridBagConstraints();
+		GridBagConstraints gbcStatus = new GridBagConstraints();
+		GridBagConstraints gbcSide = new GridBagConstraints();
+		
+//		gbcStatus.insets = new Insets(5, 5, 5, 5);
+		
+		gbcMaze.fill = GridBagConstraints.BOTH;
+		gbcSide.fill = GridBagConstraints.BOTH;
+		gbcSide.fill = GridBagConstraints.BOTH;
+		
+		gbcStatus.gridy = 0;
+		gbcStatus.gridx = 0;
+		gbcStatus.gridheight = 1;
+		gbcStatus.gridwidth = 10;
+		gbcStatus.weightx = 0.5;
+		gbcStatus.weighty = 0.01;
+		
+		gbcMaze.gridy = 1;
+		gbcMaze.gridx = 0;
+		gbcMaze.gridheight = 10;
+		gbcMaze.gridwidth = 10;
+		gbcMaze.weightx = 1.0;
+		gbcMaze.weighty = 1.0;
+		
+		gbcSide.gridy = 1;
+		gbcSide.gridx = 10;
+		gbcSide.gridheight = 11;
+		gbcSide.gridwidth = 2;
+		gbcSide.weightx = 0.1;
+		gbcSide.weighty = 0.45;
+		
+		this.add(mazePanel, gbcMaze);
+		this.add(statusBar, gbcStatus);
+		this.add(sideMenu, gbcSide);
+		
+		
+		
+		
+		
+//		gLayout.setAutoCreateGaps(true);
+//		gLayout.setAutoCreateContainerGaps(true);
+		
+//		gLayout.setHorizontalGroup(
+//				gLayout.createParallelGroup(Alignment.LEADING)
+//					.addGroup(Alignment.TRAILING, gLayout.createSequentialGroup()
+//						.addContainerGap()
+//						.addGroup(gLayout.createParallelGroup(Alignment.LEADING)
+//							.addComponent(mazePanel, GroupLayout.PREFERRED_SIZE, 280, GroupLayout.PREFERRED_SIZE)
+//							.addComponent(statusBar, GroupLayout.PREFERRED_SIZE, 280, GroupLayout.PREFERRED_SIZE)
+//						)
+//						.addPreferredGap(ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
+//						.addComponent(sideMenu, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+//						.addContainerGap()
+//					)
+//		);
+//		gLayout.setVerticalGroup(
+//				gLayout.createParallelGroup(Alignment.LEADING)
+//					.addGroup(Alignment.TRAILING, gLayout.createSequentialGroup()
+//						.addContainerGap()
+//						.addGroup(gLayout.createParallelGroup(Alignment.TRAILING)
+//							.addGroup(gLayout.createSequentialGroup()
+//								.addComponent(statusBar, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+//								.addPreferredGap(ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+//								.addComponent(mazePanel, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)
+//							)
+//							.addComponent(sideMenu, GroupLayout.DEFAULT_SIZE, 278, Short.MAX_VALUE)
+//						)
+//						.addContainerGap()
+//					)
+//		);
+		
+//		gLayout.setHorizontalGroup(
+//				gLayout.createSequentialGroup()
+//					
+//					.addGroup(gLayout.createParallelGroup()
+//						.addComponent(statusBar)
+//						.addComponent(mazePanel)
+//					)
+//					
+//		);
+//		
+//		gLayout.setVerticalGroup(
+//				gLayout.createSequentialGroup()
+//					.addGroup(gLayout.createParallelGroup()
+//						.addComponent(statusBar)
+//					)
+//					.addComponent(mazePanel)
+//		);
 		
 		// initialise stats
 		currLevel = 1;
@@ -163,11 +261,11 @@ public class GameScreen extends JPanel implements ActionListener {
 		JLabel hintsRemaining = new JLabel("Hints Remaining: ");
 		statusBar.add(hintsRemaining, BorderLayout.EAST);
 
-		add(statusBar, BorderLayout.NORTH);
+		//add(statusBar, BorderLayout.NORTH);
 	}
 
 	private void initSideMenu() {
-		JPanel sideMenu = new JPanel();
+		sideMenu = new JPanel();
 		GroupLayout groupLayout = new GroupLayout(sideMenu);
 		sideMenu.setLayout(groupLayout);
 
@@ -229,6 +327,6 @@ public class GameScreen extends JPanel implements ActionListener {
 
 		groupLayout.linkSize(hintButton, pauseButton, menuButton); // keep size of buttons consistent
 
-		add(sideMenu, BorderLayout.EAST);
+		//add(sideMenu, BorderLayout.EAST);
 	}
 }
