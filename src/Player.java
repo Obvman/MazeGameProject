ecd46421045f664cafcebca945aef41820e75f0a
leon.ng9@outlook.com <leon-ng9@outlook.com>
2016-05-25 11:51:03 +1000

    Remove multiple Spell classes

--- src/Player.java
+++ src/Player.java
@@ -241,35 +241,13 @@ public class Player implements MovableSprite, ActionListener {
 				int imageWidth = getImage().getWidth(null);
 				int imageHeight = getImage().getHeight(null);
 				
-				if (spellType == 1) {
-					spells.add(new WaterSpell(getX() + lastDX * imageWidth, 
-							getY() + lastDY * imageHeight, 2*lastDX, 2*lastDY));
-					playSound("resources/sound/water.wav");
-					
-				} else if (spellType == 2) {
-					spells.add(new FireSpell(getX() + lastDX * imageWidth, 
-							getY() + lastDY * imageHeight, 2*lastDX, 2*lastDY));
-					playSound("resources/sound/fire.wav");
-				} else if (spellType == 3) {
-					spells.add(new AirSpell(getX() + lastDX * imageWidth, 
-							getY() + lastDY * imageHeight, 2*lastDX, 2*lastDY));
-					playSound("resources/sound/wind.wav");
-				}
+				spells.add(new Spell(getX() + lastDX * imageWidth, 
+						getY() + lastDY * imageHeight, 2*lastDX, 2*lastDY, spellType));
 			}
 		}
 	}
 
-	private void playSound(String soundName) {
-	       try 
-	       {
-	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
-	        Clip clip = AudioSystem.getClip();
-	        clip.open(audioInputStream);
-	        clip.start();
-	       }
-	       catch(Exception ex){
-	       }
-	}
+	
 
 	private BufferedImage getScaledImage(BufferedImage src, int w, int h){
 		int finalw = w;
--- src/Spell.java
+++ src/Spell.java
@@ -1,5 +1,121 @@
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.io.File;
 
-public interface Spell extends MovableSprite {
-	int getStage();
-	void updatePosition();
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.ImageIcon;
+
+public class Spell implements MovableSprite {
+
+	private int initialX;
+	private int initialY;
+	double x;
+	double y;
+	private int dx;
+	private int dy;
+	private Image image1;
+	private Image image2;
+	private Image image3;
+	private int stage; 
+
+	public Spell(int startX, int startY, int dx, int dy, int spellType) {
+		initialX = startX;
+		initialY = startY;
+		
+		x = startX;
+		y = startY;
+		this.dx = dx;
+		this.dy = dy;
+		
+		int scaledSize = (3 * Maze.MAZE_CELL_SIZE) / 4;
+		
+		if (spellType == GameScreen.FIRE) {
+			image1 = (new ImageIcon("resources/32_flame_1.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			image2 = (new ImageIcon("resources/32_flame_2.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			image3 = (new ImageIcon("resources/32_flame_3.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			playSound("resources/sound/fire.wav");
+		} else if (spellType == GameScreen.WATER) {
+			image1 = (new ImageIcon("resources/water1.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			image2 = (new ImageIcon("resources/water2.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			image3 = (new ImageIcon("resources/water3.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			playSound("resources/sound/water.wav");
+		} else if (spellType == GameScreen.AIR) {
+			image1 = (new ImageIcon("resources/air1.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			image2 = (new ImageIcon("resources/air2.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			image3 = (new ImageIcon("resources/air3.png")).getImage().getScaledInstance(scaledSize, scaledSize, Image.SCALE_SMOOTH);
+			playSound("resources/sound/wind.wav");
+		}
+		
+	}
+	
+	@Override
+	public int getX() {
+		return (int)x;
+	}
+
+	@Override
+	public int getY() {
+		return (int)y;
+	}
+
+	@Override
+	public int getDX() {
+		return dx;
+	}
+
+	@Override
+	public int getDY() {
+		return dy;
+	}
+
+	@Override
+	public Image getImage() {
+		if (stage == 0) {
+			return image1;
+		} else if (stage == 1) {
+			return image2;
+		} else {
+			return image3;
+		}
+	}
+	
+	@Override
+	public Rectangle getBounds() {
+		return new Rectangle(getX(), getY(), getImage().getWidth(null), getImage().getHeight(null));
+	}
+	
+	public void updatePosition() {
+		x += dx * (double)Maze.MAZE_CELL_SIZE/32;
+		y += dy * (double)Maze.MAZE_CELL_SIZE/32;
+		
+		stage = (int)Math.sqrt(Math.pow(x - initialX, 2) + Math.pow(y - initialY, 2))/getImage().getWidth(null);
+	}
+	
+	public int getStage() {
+		return stage;
+	}
+	
+	public void updateStage() {
+		stage++;
+	}
+
+	@Override
+	public void manualMove(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+	
+	private void playSound(String soundName) {
+	       try 
+	       {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	       }
+	       catch(Exception ex){
+	       }
+	}
 }
