93824ae0cd98f26fe5791ce4f729e2d6a635c799
leon-ng9 <leon.ng9@outlook.com>
2016-05-27 12:40:32 +1000

    Remove warnings

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,10 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
